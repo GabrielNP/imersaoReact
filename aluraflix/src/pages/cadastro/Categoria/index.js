6d7ba78070564b7e4a75b30b4b5616de7de75635
GabrielNP <gabrielnp.2010@gmail.com>
2020-08-02 22:46:40 -0300

    Reusable forms; React states

--- aluraflix/src/pages/cadastro/Categoria/index.js
+++ aluraflix/src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import PageDefault from '../../../components/PageDefault'
+import FormField from '../../../components/FormField'
 
 function CadastroCategoria() {
+  const [categories, setCategories] = useState([])
+
+  const initialValues = {
+    categoryName: '',
+    description: '',
+    color: ''
+  }
+
+  const [values, setValues] = useState(initialValues)
+  function setValue(key, value) {
+     setValues({
+       ...values,
+       [key]: value
+     });
+  }
+
+  function handleChange(event) {
+    setValue(
+      event.target.getAttribute('name'),
+      event.target.value
+    )
+  }
+
+  
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.categoryName}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(event) {
+        event.preventDefault()
+        setCategories([
+          ...categories,
+          values
+        ])
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+        setValues(initialValues)
+        
+      }}>
+
+        <FormField 
+          label="Nome da Categoria"
+          value={values.categoryName}
+          name='categoryName'
+          onChange={handleChange}
+        />
+
+        <FormField 
+          label="Descrição"
+          type='textarea'
+          value={values.description}
+          name='description'
+          onChange={handleChange}
+        />
+
+        <FormField 
+          label="Cor"
+          type='color'
+          value={values.color}
+          name='color'
+          onChange={handleChange}
+        />
 
         <button>
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categories.map((category, index) => {
+          return (
+            <li key={`${category}${index}`}>
+              {category.categoryName}
+            </li>
+          )
+        })}
+      </ul>
 
 
       <Link to="/">
@@ -29,4 +90,4 @@ function CadastroCategoria() {
   )
 }
 
-export default CadastroCategoria;
+export default CadastroCategoria
